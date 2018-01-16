f0d1a3bde01da8d13077141c6782519f83fb7821
SpencerWhitehead7@gmail.com <spencerwhitehead7@gmail.com>
2018-01-16 03:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "AAA 170"
-var height = "48"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height)+1)
+console.log(height+1)
